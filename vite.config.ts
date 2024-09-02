2f50b63602ea393d14ac73fd3ae76e7f1431e033
Andrew Dauphinais <andrew.dauphinais@konverse.com>
2024-09-02 13:28:51 -0500

    Work in progress trying to get multiple entry points working

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
@@ -6,21 +6,42 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "react-jp-ui",
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "css-template-components",
       fileName: (format) => `index.${format}.js`,
+      formats: ["es"],
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "vue"],
+      input: {
+        server: resolve(__dirname, "src/server/index.ts"),
+        client: resolve(__dirname, "src/client/index.ts"),
+      },
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
         },
+        inlineDynamicImports: false,
       },
     },
     sourcemap: true,
     emptyOutDir: true,
   },
   plugins: [react(), dts()],
+  resolve: { preserveSymlinks: true },
 });
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         server: resolve(__dirname, "./src/server/index.ts"),
+//         client: resolve(__dirname, "./src/client/index.ts"),
+//       },
+//       output: {
+//         preserveModules: false,
+//       },
+//     },
+//   },
+// });
